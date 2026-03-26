54559d30eff7cfac42148e6370d50a42e3736eb5
computing-AU <computing@avantika.edu.in>
2026-03-26 11:57:59 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,7 @@
+X ml of water today. Determine if the client followed the doctor's advice or not.
+Input Format
+
+The first line contains a single integer 
+T — the number of test cases. Then the test cases follow.
+The first and only line of each test case contains one integer 
+X — the amount of water client drank today.
