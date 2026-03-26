7167ce5ffee572d3ffc8bec1131b594feaba502a
computing-AU <computing@avantika.edu.in>
2026-03-26 07:33:06 +0530

    Add files via upload

---
+++ Problem_4.py
@@ -0,0 +1,33 @@
+"""   Turn Around Function 
+
+Design a Function that should take list as an argument and perform turnaround() it in following fasion.
+
+Input Format:
+
+1. Take M, Inputs for Turnaround
+2. Enter M numbers on new line 
+
+10
+10
+20
+30
+40
+50
+60
+70
+80
+90
+
+Output Format:
+50
+60
+70
+80
+90
+100
+30
+40
+10
+20
+
+"""
---
+++ Problem_5.py
@@ -0,0 +1,29 @@
+""" 
+
+You have been given an ilist of size M that contains 1 and 2. Write a function to arrange them in order.
+function name=arrange() 
+#You are not allowed to use sort function in this program
+
+Sample Input 1:
+1
+7
+1 2 2 2 1 2 2
+
+Sample Output 1:
+
+1 1 2 2 2 2 2
+
+
+Sample Input 2:
+2
+8
+2 1 2 2 1 2 1 2
+5
+1 2 1 2 1
+
+Sample Output 2:
+1 1 1 2 2 2 2 2
+
+1 1 1 2 2 
+
+"""
